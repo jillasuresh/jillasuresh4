e56b12bfe0f1ffbe54948b88dd1c154784f6448c
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-17 12:54:17 +0530

    Update longnumber.py

--- longnumber.py
+++ longnumber.py
@@ -1,9 +1,9 @@
 a=int(input(""))
 b=int(input(""))
 c=int(input(""))
-if(a>b and a>c)
+if(a<b and a<c)
    print("larger")
-elif(b>c and b>a)
+elif(b<c and b<a)
    print("larger")
 else:
    print("larger")
