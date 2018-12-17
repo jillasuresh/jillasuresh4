48438da2b5c07264f501f0e60483e016ed25bfd5
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-17 12:33:20 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
 n1=input('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-if(n1=='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+if(n1)
    print("alphabet")
 else:
    print("no")
