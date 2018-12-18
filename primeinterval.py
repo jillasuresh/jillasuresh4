2b634586a4e48ce8691bd3ca9322066d20152cd0
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-18 11:30:03 +0530

    Update primeinterval.py

--- primeinterval.py
+++ primeinterval.py
@@ -1,6 +1,5 @@
-lower case=int(input(""))
-upper case=int(input(""))
-for i in range(lower case,upper case+1):
+i=int(input(""))
+for i in range(i+1):
 if i>1:
    for i in range(2,int(n/2)):
    if(n%i!=0):
