19784582b6ca4cd9118fd75176578d87c93655d9
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-18 10:42:40 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
-n= int(input(""))
- for i in range(2, int(n/2)):
-    if n% i== 0:
-       print("not prime number")
-       break
+n= int(input("")
+for i in range(2, int(n/2)):
+   if n% i== 0:
+      print("not prime number")
+      break
 else:
     print("prime number")
