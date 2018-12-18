3fceb8ba9d21b217275fd054512b1b5264ccff8c
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-18 12:20:10 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n==1:
+     return n
+    else:
+      return n*factorial(n-1)
+n=int(input(""))
+if (n<0):
+   print("Factorial negative numbers")
+elif n==0:
+   print("Factorial of 0 is 1")
+else:
+   print("factorial(n)")
