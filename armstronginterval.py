49e0b8976080a574b2c604ead29b06f952d8a1ba
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-18 12:06:41 +0530

    Update armstronginterval.py

--- armstronginterval.py
+++ armstronginterval.py
@@ -2,10 +2,10 @@ lower case=int(input(""))
 upper case=int(input(""))
  for n in range(lower case,upper case+1):
   sum = 0
- temp = num
+  temp=n
    while temp>0:
        digit=temp%10
-       sum+= digit ** 3
+       sum+= digit**3
        temp//=10
  if n==sum:
        print(n)
