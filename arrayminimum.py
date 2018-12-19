cd440bc4c5e4139d09bc1f56047e0adb15abb13a
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-19 10:37:35 +0530

    Update arrayminimum.py

--- arrayminimum.py
+++ arrayminimum.py
@@ -2,7 +2,7 @@ def Nminelements(list1,N):
     final_list =[]; 
     for i in range(0,N):     
         min1=9999999; 
-           for j in range(len(list1)):       
+          for j in range(len(list1)):       
             if list1[j]<min1: 
                 min1 = list1[j]; 
                 list1.remove(min1); 
