62c74aa79591499fc64991814ccb6bfb34e33524
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-19 11:35:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 def reorder(arr,index,n): 
   temp=[0]*n; 
-   for i in range(0,n): 
+  for i in range(0,n): 
         temp[index[i]]=arr[i] 
-   for i in range(0,n): 
-        arr[i]=temp[i] 
-        index[i]=i 
+  for i in range(0,n): 
+       arr[i]=temp[i] 
+       index[i]=i 
 arr=[50, 40, 70, 60, 90] 
 index=[3, 0, 4, 1, 2] 
 n=len(arr) 
