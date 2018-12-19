08d798ddff200dba23ff9a115d19a041d34ea834
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-19 12:03:04 +0530

    Update subtime.py

--- subtime.py
+++ subtime.py
@@ -1,3 +1,6 @@
-from datetime import datetime,timedelta
-d = datetime.today()- timedelta(hours=0,minutes=50)
-d.strftime("")
+from datetime import datetime
+s1='10:04:00'
+s2='11:03:11'
+format=%H:%M:%S
+time=datetime.strptime(s2,format)-datetime.strptime(s1,format)
+print time
