4bccdfe8f54171446c064cfec947e6ba422e8187
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-19 10:48:07 +0530

    Create sortarray.py

---
+++ sortarray.py
@@ -0,0 +1,9 @@
+def sortit(arr,n): 
+    for i in range(n): 
+        arr[i] = i+1
+  if __name__=='__main__': 
+    arr = [10,7,9,2,8,3,5,4,6,1] 
+    n = len(arr) 
+   sortit(arr,n) 
+  for i in range(n): 
+      print(arr[i],end=" ") 
