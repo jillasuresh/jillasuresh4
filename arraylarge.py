e3076a72e056eef6190c9433fa7209be88858a94
jillasuresh <45929499+jillasuresh@users.noreply.github.com>
2018-12-19 10:27:43 +0530

    Update arraylarge.py

--- arraylarge.py
+++ arraylarge.py
@@ -1,8 +1,8 @@
 def Nmaxelements(list1,N): 
     final_list = [] 
-  
-    for i in range(0,N):  
+   for i in range(0,N):  
         max1=0
+        for j in range(len(list1)):
        if list1[j]>max1: 
                 max1=list1[j]; 
                    list1.remove(max1); 
